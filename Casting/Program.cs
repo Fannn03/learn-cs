147970a9f6a3c25cb204a96c19f52c592fd89bfe
Fannn03 <fann.fann771@gmail.com>
2023-11-21 08:33:41 +0700

    explicit casting

--- Casting/Program.cs
+++ Casting/Program.cs
@@ -3,18 +3,21 @@
 namespace Casting {
   class Casting {
     static void Main (string[] args) {
-      // Implicit casting > automatic casting
+      // Implicit casting > automatic casting > convert from smaller to larger
       int age = 20;
       double myAge = age;
 
       Console.WriteLine(age);
       Console.WriteLine(myAge);
 
+      // Explicit casting > manual casting > convert from larger to smaller
       double yourAge = 9.12;
-      int mineAge = yourAge;
+      int mineAge = (int) yourAge;
+      char charAge = (char) mineAge;
 
       Console.WriteLine(yourAge);
       Console.WriteLine(mineAge);
+      Console.WriteLine(charAge);
     }
   }
 }
--- Casting/obj/Debug/net8.0/Casting.AssemblyInfo.cs
+++ Casting/obj/Debug/net8.0/Casting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Casting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f38b551c8afd2421e22ee35f4dc0a8ae77bfbbbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Casting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Casting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
