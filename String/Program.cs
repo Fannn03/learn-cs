54a40b77f154d8a10051445011645682b62033c1
Fannn03 <fann.fann771@gmail.com>
2023-11-21 09:02:07 +0700

    String manipulation

---
+++ String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace String {
+  class StringProgram {
+    static void Main (string[] args) {
+      const string name = "Irfan Setiawan";
+
+      Console.WriteLine(name);
+      Console.WriteLine(name.Length);
+      Console.WriteLine(name.ToUpper());
+
+      // String interpolation same as string format text in javascript, add '$' before double quote
+      Console.WriteLine($"My Name is {name}");
+      Console.WriteLine(name[1]); // will print r
+      Console.WriteLine(name.IndexOf("a")); // will print 3, cause they will find first index character
+      Console.WriteLine(name.Substring(name.IndexOf("S")));
+    }
+  }
+}
