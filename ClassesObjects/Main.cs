8e253263ed5300aaaf9984dfd2e1f3e59a153e34
Fannn03 <fann.fann771@gmail.com>
2023-11-21 15:14:11 +0700

    class object

---
+++ ClassesObjects/Main.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Program {
+  class Apple {
+    public string name = "Apple";
+
+  }
+}
---
+++ ClassesObjects/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program {
+  class Fruits {
+
+    bool isJuicy = true;
+    string name = "fruits";
+    int addValue;
+
+    // static method can be accessed without creating an object of the class
+    private void buyFruits () {
+      Console.WriteLine("Buy me some fruits");
+    }
+
+    static void Main (string[] args) {
+
+      Fruits fruits = new Fruits();
+      Apple apple = new Apple(); // create object from class apple
+
+      fruits.addValue = 12;
+
+      Console.WriteLine($"I Like fruits, because it's juicy {fruits.isJuicy}");
+      Console.WriteLine($"Name Fruits {fruits.name}");
+      Console.WriteLine($"New Value {fruits.addValue}");
+
+      fruits.buyFruits();
+
+      Console.WriteLine(apple.name);
+
+    }
+  }
+}
