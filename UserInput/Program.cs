92128c2d7a381c82f0d0cbfc8d95053e5f5470d7
Fannn03 <fann.fann771@gmail.com>
2023-11-21 08:47:38 +0700

    user input

---
+++ UserInput/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserInput {
+  class UserInputProgram {
+    static void Main (string[] args) {
+      // will give warning if input value is null
+      Console.Write("Enter your name : ");
+      string name = Console.ReadLine();
+
+      Console.Write("Enter your Age : ");
+      int age = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("Your name : " + name);
+      Console.WriteLine("Your age : " + age);
+    }
+  }
+}
