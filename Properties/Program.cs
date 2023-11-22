3101e9affca3ff9db70088a1747f8adfa56aa8a8
Fannn03 <fann.fann771@gmail.com>
2023-11-22 15:00:29 +0700

    add property set get

---
+++ Properties/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Program;
+
+class Person {
+  // public field
+  public string name;
+  // constructor
+  public Person (string personName) {
+    name = personName;
+  }
+
+  private int age;
+  // property access private field 'age'
+  public int Age {
+    get { return age; }
+    set { age = value; }
+  }
+
+  // property short hand
+  public int newAge
+  {get; set;}
+}
+
+class Program {
+  static void Main (string[] args) {
+    Person person = new Person("Irfan");
+    Console.WriteLine(person.name);
+
+    person.Age = 12;
+    Console.WriteLine(person.Age);
+
+    person.newAge = 25;
+    Console.WriteLine(person.newAge);
+  }
+}
