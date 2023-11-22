c5143e5de3f9cb15b60482f804a2d5d4174f2a6e
Fannn03 <fann.fann771@gmail.com>
2023-11-22 16:39:40 +0700

    inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program;
+
+sealed class Person {
+  public string name = "Using sealed keyword cannot have inheritance class";
+}
+
+class Fruits {
+  public string PropertyFruit 
+  {get; set;}
+
+  public void Fruit () {
+    Console.WriteLine("Fruits is juicy");
+  }
+}
+
+class Apple : Fruits {
+  public string name = "apple";
+}
+
+class Program {
+  static void Main (string[] args) {
+    Apple apple = new Apple();
+    apple.Fruit();
+
+    Console.WriteLine(apple.name);
+
+    apple.PropertyFruit = "This is property fruits";
+    Console.WriteLine(apple.PropertyFruit);
+  }
+}
