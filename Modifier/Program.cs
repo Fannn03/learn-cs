831914a8eae0cada5cb3fe4e6ac813500792b696
Fannn03 <fann.fann771@gmail.com>
2023-11-22 14:40:25 +0700

    access modifiers

---
+++ Modifier/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program;
+
+class Person {
+  // can't be accessed in class People
+  private string name = "irfan";
+
+  // can be accessed in class people
+  public string publicName = "irfan";
+}
+
+class People {
+  static void Main (string[] args) {
+    
+    Person person = new Person();
+
+    Console.WriteLine(person.publicName);
+
+  }
+}
